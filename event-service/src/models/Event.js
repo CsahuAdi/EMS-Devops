0e79470cbf506ecac574748ab50a5c79e97422de
Devansh Sarda <sardadevansh05@gmail.com>
2026-05-01 16:50:06 +0530

    Added event service

---
+++ event-service/src/models/Event.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    date: {
+      type: Date,
+      required: true,
+    },
+    venue: String,
+    capacity: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
