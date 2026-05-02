f2de0b1e898c63f49eb4014bd91576d394e2bbdd
CsahuAdi <chiranjancs01032006@gmail.com>
2026-05-02 11:44:19 +0530

    docker and kubernetes works

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+  import axios from "axios";
 
-const EVENT_API = "http://localhost:5000";
-const REG_API = "http://localhost:5002";
+  const EVENT_API = "http://192.168.49.2:30500";
+  const REG_API = "http://192.168.49.2:30502";
 
-export const getEvents = () => axios.get(`${EVENT_API}/events`);
-export const getEventById = (id) => axios.get(`${EVENT_API}/events/${id}`);
-export const createEvent = (data) => axios.post(`${EVENT_API}/events`, data);
-export const updateEvent = (id, data) =>
-  axios.put(`${EVENT_API}/events/${id}`, data);
-export const deleteEvent = (id) =>
-  axios.delete(`${EVENT_API}/events/${id}`);
+  export const getEvents = () => axios.get(`${EVENT_API}/events`);
+  export const getEventById = (id) => axios.get(`${EVENT_API}/events/${id}`);
+  export const createEvent = (data) => axios.post(`${EVENT_API}/events`, data);
+  export const updateEvent = (id, data) =>
+    axios.put(`${EVENT_API}/events/${id}`, data);
+  export const deleteEvent = (id) =>
+    axios.delete(`${EVENT_API}/events/${id}`);
 
-export const registerUser = (data) =>
-  axios.post(`${REG_API}/registrations`, data);
+  export const registerUser = (data) =>
+    axios.post(`${REG_API}/registrations`, data);
 
-export const getRegistrations = (eventId) =>
-  axios.get(`${REG_API}/registrations/event/${eventId}`);
+  export const getRegistrations = (eventId) =>
+    axios.get(`${REG_API}/registrations/event/${eventId}`);
